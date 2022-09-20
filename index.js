421916ca5633cf44f23e66ee96df6df7d21eb5fb
Glazelf <gideon.steenbergen@gmail.com>
2022-09-20 18:51:46 +0200

    Add initial script

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
-const config = require("./config.json");
-const net = require("net");
-const port = 6000;
-const host = config.switchIP; // change to switch's IP
+async function tableturfidler() {
+    const config = require("./config.json");
+    const net = require("net");
+    const port = config.port;
+    const host = config.ip; // change to switch's IP
 
-const conn = net.createConnection(port, host); // connect
-conn.setEncoding("utf-8"); // sends all commands as utf-8 (same as .encode())
+    const conn = net.createConnection(port, host); // connect
+    conn.setEncoding("utf-8"); // sends all commands as utf-8 (same as .encode())
 
-const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+    const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
-let endless = true; // set to false to stop the loop
-while (endless === true) {
+    conn.write("detachController \r\n"); // detach from console
+    console.log("Detached controller from console.");
+    await sleep(5000);
+    // Reconnect
+    conn.write("click A \r\n");
+    conn.write("click A \r\n");
+    console.log("Connected controller to console.");
 
+    while (true) {
+        await sleep(5000);
+        console.log("Choosing deck.");
+        conn.write("click A \r\n");
+        await sleep(10000);
+        console.log("Holding steady.");
+        conn.write("click A \r\n");
+        await sleep(5000);
+        console.log("Starting turn loop.");
+        for (i = 0; i < 12; i++) {
+            conn.write("click DDOWN \r\n");
+            conn.write("click DDOWN \r\n");
+            conn.write("click A \r\n");
+            conn.write("click A \r\n");
+            console.log(`Passed turn ${i + 1}.`);
+            await sleep(10000);
+        };
+        console.log("Waiting for results...");
+        await sleep(5000);
+        console.log("Look at all that EXP!");
+        conn.write("click A \r\n");
+        await sleep(10000);
+        console.log("Restarting battle!");
+        conn.write("click A \r\n");
+    };
 };
+tableturfidler();
